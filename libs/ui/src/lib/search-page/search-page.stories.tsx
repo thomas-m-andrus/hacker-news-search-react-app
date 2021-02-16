c678e027fa8edea97527f14c4adaa01594cc5381
Thomas Andrus <thomasmandrus@gmail.com>
2021-02-16 14:45:07 -0600

    added a story

---
+++ libs/ui/src/lib/search-page/search-page.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SearchPage } from './search-page';
+
+export default {
+  component: SearchPage,
+  title: 'SearchPage',
+};
+
+export const primary = () => {
+  return <SearchPage />;
+};
