59ca3957c86e8a62cbc696d3ff1a8c7b24f76667
Thomas Andrus <thomasmandrus@gmail.com>
2021-02-16 15:42:57 -0600

    got all the elements in the component

--- libs/ui/src/lib/search-box/search-box.tsx
+++ libs/ui/src/lib/search-box/search-box.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import { SearchBoxProps } from '@hacker-news-search-react-app/types';
 import './search-box.module.scss';
 import TextField from '@material-ui/core/TextField';
 import { Autocomplete } from '@material-ui/lab';
 import Button from '@material-ui/core/Button';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
 export function SearchBox(props: SearchBoxProps) {
   return (
@@ -18,6 +20,44 @@ export function SearchBox(props: SearchBoxProps) {
         )}
       />
       <Button size="large">Search</Button>
+      <div>
+        <div>Search</div>
+        <Select>
+          {[
+            { label: 'something', value: 'hey' },
+            { label: 'something else', value: 'hey hey' },
+            { label: 'something again', value: 'hey hey hey' },
+          ].map(
+            ({ label, value }): ReactElement => (
+              <MenuItem value={value}>{label}</MenuItem>
+            )
+          )}
+        </Select>
+        <div>by</div>
+        <Select>
+          {[
+            { label: 'something', value: 'hey' },
+            { label: 'something else', value: 'hey hey' },
+            { label: 'something again', value: 'hey hey hey' },
+          ].map(
+            ({ label, value }): ReactElement => (
+              <MenuItem value={value}>{label}</MenuItem>
+            )
+          )}
+        </Select>
+        <div>from</div>
+        <Select>
+          {[
+            { label: 'something', value: 'hey' },
+            { label: 'something else', value: 'hey hey' },
+            { label: 'something again', value: 'hey hey hey' },
+          ].map(
+            ({ label, value }): ReactElement => (
+              <MenuItem value={value}>{label}</MenuItem>
+            )
+          )}
+        </Select>
+      </div>
     </div>
   );
 }
