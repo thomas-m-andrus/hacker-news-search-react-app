7b87a8ebe0c0f220d28242f4a46dc666782045f0
Thomas Andrus <thomasmandrus@gmail.com>
2021-02-16 16:29:09 -0600

    Added some styling

--- libs/ui/src/lib/search-box/search-box.tsx
+++ libs/ui/src/lib/search-box/search-box.tsx
@@ -3,68 +3,80 @@ import { SearchBoxProps } from '@hacker-news-search-react-app/types';
 import './search-box.module.scss';
 import TextField from '@material-ui/core/TextField';
 import { Autocomplete } from '@material-ui/lab';
-import Button from '@material-ui/core/Button';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
+import {
+  Button,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
+} from '@material-ui/core';
 
 export function SearchBox(props: SearchBoxProps) {
   return (
     <div className={`search-box`}>
-      <Autocomplete
-        id="combo-box-demo"
-        options={top100Films}
-        getOptionLabel={(option) => option}
-        style={{ width: 300 }}
-        renderInput={(params) => (
-          <TextField {...params} label="Combo box" variant="outlined" />
-        )}
-      />
-      <Button className={`search-box__button`} size="large">
-        Search
-      </Button>
+      <div className={`search-box__input-group`}>
+        <Autocomplete
+          id="combo-box-demo"
+          options={top100Films}
+          getOptionLabel={(option) => option}
+          style={{ width: 300 }}
+          renderInput={(params) => (
+            <TextField {...params} label="Combo box" variant="outlined" />
+          )}
+        />
+        <Button className={`search-box__button`} size="large">
+          Search
+        </Button>
+      </div>
       <div className={`search-box__filter`}>
-        <div className={`search-box__filter-label`}>Search</div>
-        <Select
-          className={`search-box__filter-select search-box_filter-select--type`}
-        >
-          {[
-            { label: 'something', value: 'hey' },
-            { label: 'something else', value: 'hey hey' },
-            { label: 'something again', value: 'hey hey hey' },
-          ].map(
-            ({ label, value }): ReactElement => (
-              <MenuItem value={value}>{label}</MenuItem>
-            )
-          )}
-        </Select>
-        <div className={`search-box__filter-label`}>by</div>
-        <Select
-          className={`search-box__filter-select search-box_filter-select--rank`}
-        >
-          {[
-            { label: 'something', value: 'hey' },
-            { label: 'something else', value: 'hey hey' },
-            { label: 'something again', value: 'hey hey hey' },
-          ].map(
-            ({ label, value }): ReactElement => (
-              <MenuItem value={value}>{label}</MenuItem>
-            )
-          )}
-        </Select>
-        <div>from</div>
-        <Select
-          className={`search-box__filter-select search-box_filter-select--time`}
-        >
-          {[
-            { label: 'something', value: 'hey' },
-            { label: 'something else', value: 'hey hey' },
-            { label: 'something again', value: 'hey hey hey' },
-          ].map(
-            ({ label, value }): ReactElement => (
-              <MenuItem value={value}>{label}</MenuItem>
-            )
-          )}
-        </Select>
+        {[
+          {
+            label: 'Search',
+            type: 'type',
+            options: [
+              { label: 'something', value: 'hey' },
+              { label: 'something else', value: 'hey hey' },
+              { label: 'something again', value: 'hey hey hey' },
+            ],
+          },
+          {
+            label: 'by',
+            type: 'rank',
+            options: [
+              { label: 'something', value: 'hey' },
+              { label: 'something else', value: 'hey hey' },
+              { label: 'something again', value: 'hey hey hey' },
+            ],
+          },
+          {
+            label: 'from',
+            type: 'time',
+            options: [
+              { label: 'something', value: 'hey' },
+              { label: 'something else', value: 'hey hey' },
+              { label: 'something again', value: 'hey hey hey' },
+            ],
+          }
+        ].map(
+          ({ label: titleLabel, options, type }): ReactElement => (
+            <FormControl>
+              <InputLabel id={`${type.toLowerCase()}-select-label`}>
+                {titleLabel}
+              </InputLabel>
+              <Select
+                labelId={`${type.toLowerCase()}-select-label`}
+                label={titleLabel}
+                className={`search-box__filter-select search-box__filter-select--${type}`}
+              >
+                {options.map(
+                  ({ label, value }): ReactElement => (
+                    <MenuItem value={value}>{label}</MenuItem>
+                  )
+                )}
+              </Select>
+            </FormControl>
+          )
+        )}
       </div>
     </div>
   );
