3eb333d15ceea98f489c9527a6994ad0c4a8c9c7
Thomas Andrus <thomasmandrus@gmail.com>
2021-02-16 23:34:15 -0600

    added styling

--- libs/ui/src/lib/story-row/story-row.stories.tsx
+++ libs/ui/src/lib/story-row/story-row.stories.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { StoryRow } from './story-row';
 import data from '../../../../../mocks/stephen-hawking-has-died.json';
+import { List } from '@material-ui/core';
 
 export default {
   component: StoryRow,
   title: 'StoryRow',
 };
 
-export const primary = () => {
-  return <StoryRow story={data.hits[0]} />;
+export const titleMatched = () => {
+  return (
+    <List>
+      <StoryRow story={data.hits[0]} />
+    </List>
+  );
+};
+export const titleNotMatched = () => {
+  return (
+    <List>
+      <StoryRow story={data.hits[1]} />
+    </List>
+  );
 };
--- libs/ui/src/lib/story-row/story-row.tsx
+++ libs/ui/src/lib/story-row/story-row.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import { StoryRowProps } from '@hacker-news-search-react-app/types';
 import './story-row.module.scss';
+import {
+  ListItem,
+  ListItemProps,
+  Typography,
+  ListItemText,
+} from '@material-ui/core';
+import parse from 'html-react-parser';
 
-export function StoryRow({ story }: StoryRowProps) {
+function ListItemLink(props: ListItemProps<'a', { button?: true }>) {
+  return <ListItem button component="a" {...props} />;
+}
+
+export function StoryRow({
+  story: { author, title, url, num_comments, _highlightResult },
+}: StoryRowProps) {
+  const MatchedTitle =
+    Object.prototype.hasOwnProperty.call(_highlightResult, 'title') &&
+    _highlightResult.title.matchLevel === 'full';
+  const displayTitle = MatchedTitle ? _highlightResult.title.value : title;
   return (
-    <div>
-      <h1>Welcome to story-row!</h1>
-    </div>
+    <ListItemLink className="story-row" href={url}>
+      <ListItemText>
+        <Typography variant="h6" component="h3" align="left">
+          {MatchedTitle ? parse(displayTitle) : displayTitle}
+        </Typography>
+        <Typography variant="subtitle1">Author: {author}</Typography>
+        <Typography variant="subtitle2">
+          Number of Comments: {num_comments}
+        </Typography>
+      </ListItemText>
+    </ListItemLink>
   );
 }
 
